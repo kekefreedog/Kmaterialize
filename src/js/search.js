1860e1cd0c41f352d85cec57fdd02ce8ddf98923
Kévin Zarshenas <kevin.zarshenas@gmail.com>
2021-12-28 16:23:05 +0100

    Clean

--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,4 @@
-/*================================================================================
-  Item Name: Materialize - Material Design Admin Template
-  Version: 5.0
-  Author: PIXINVENT
-  Author URL: https://themeforest.net/user/pixinvent/portfolio
-================================================================================*/
+
 
 var searchListLi = $(".search-list li"),
    searchList = $(".search-list"),
